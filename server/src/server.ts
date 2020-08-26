51a9ab911a39933ab9763ae2b11ffde7da89fb7d
gusta2001 <gusta.cervo14@gmail.com>
2020-08-26 16:20:44 -0300

    server chamando a rota por outro arquivo

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,8 @@
 import express from 'express';
-
+import routes from './routes';
 const app = express();
 
-app.get('/', (request, response)=> {
-    response.json([
-        "Organizadador da faculdalde",
-        "Hello Thaisinha gameplays!!!",
-        "Hello Gusta2001",
-        "Agora esta funcionando o git",
-    ]);
-})
+app.use(routes);
+
 
 app.listen(3333);
