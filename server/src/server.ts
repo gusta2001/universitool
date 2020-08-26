93ba63caaff3ba4d4aa84c0f9918206e92107ea8
gusta2001 <gusta.cervo14@gmail.com>
2020-08-25 23:15:23 -0300

    Testando o funcionamento do git

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,7 +6,8 @@ app.get('/', (request, response)=> {
     response.json([
         "Organizadador da faculdalde",
         "Hello Thaisinha gameplays!!!",
-        "Hello Gusta14",
+        "Hello Gusta2001",
+        "Agora esta funcionando o git",
     ]);
 })
 
